31d0f5c2fe206d2df3c1f9967f7914af8cf839f8
ebrah <ebrah@LAPTOP-TM8TPK8A>
2024-07-01 14:05:32 +0300

    Add project files.

---
+++ DynamicArray/Dynamicarray.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+
+namespace DynamicArray
+{
+    internal class Dynamicarray
+    {
+        int size;
+        int length;
+        int[] Array;
+
+        public Dynamicarray()
+        {
+            size = 10;
+            length = 0;
+            Array = new int[size];
+        }
+
+        //check conditions
+        public bool IsFull()
+        {
+            return (length + 1 <= size) ? false : true;
+        }
+        public bool IsEmpty()
+        {
+            return (length == 0) ? true : false;
+        }
+        // Fill array 
+        public void Append(int value)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Array is Full");
+            }
+            else
+            {
+                Array[length] = (Array[length] != 0) ? Array[length] : value;
+                length++;
+            }
+        }
+        public void insert(int value, int index)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Array is Full");
+                return;
+            }
+            else if (index >= size)
+            {
+                Console.WriteLine("OUT OF RANGE!");
+                return;
+            }
+            else if(index<0)
+            {
+                Console.WriteLine("OUT OF RANGE!");
+                return;
+            }
+            else
+            {
+                if (length <= index)
+                {
+                    Array[index] = value;
+                }
+                else
+                {
+                    for (int i = length; i >= index; i--)
+                    {
+                        Array[i + 1] = Array[i];
+                    }
+                    Array[index] = value;
+                    length++;
+                }
+            }
+        }
+        //Sorting 
+        public void sortArray()
+        {
+            int container = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length - 1; j++)
+                {
+                    if (Array[j] > Array[j + 1])
+                    {
+                        container = Array[j + 1];
+                        Array[j + 1] = Array[j];
+                        Array[j] = container;
+                    }
+                }
+            }
+        }
+        //Searching
+        public int Search(int value)
+        {
+            if (IsEmpty())
+            {
+                return -1;
+            }
+            else
+            {
+                sortArray();
+                int low = 0;
+                int high = length;
+                while (low <= high)
+                {
+                    int mid = (low + high) / 2;
+                    if (Array[mid] == value)
+                    {
+                        return mid;
+                    }
+                    else if (value < Array[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+                return -1;
+            }
+        }
+        //emptying
+        public void Remove()
+        {
+            Array[length - 1] = 0;
+            length--;
+
+        }
+        public void Delete(int value)
+        {
+            int index = Search(value);
+            if (index == -1)
+            {
+                Console.WriteLine("Not Found!");
+                return;
+            }
+            else if (index != -1)
+            {
+                Array[index] = 0;
+                for (int i = index; i < length; i++)
+                {
+                    Array[i] = Array[i + 1];
+                }
+                length--;
+            }
+        }
+        //increasing size
+        public void IncreaseSize(int NewSize)
+        {
+            int[] newArray = new int[size + NewSize];
+            for (int i = 0; i < length; i++)
+            {
+                newArray[i] = Array[i];
+            }
+            Array = newArray;
+            size += NewSize;
+        }
+        //Show Data
+        public void printArray()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                int count = 0;
+                while (count+1 <= size) 
+                {
+                    Console.WriteLine($"Index {count}:{Array[count]}");
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ DynamicArray/Program.cs
@@ -0,0 +1,102 @@
+﻿using DynamicArray;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dynamicarray a = new Dynamicarray();
+        int x,container,index;
+        do 
+        {
+            showOptions();
+            Console.WriteLine("Choose option");
+            x = Convert.ToInt32(Console.ReadLine());
+            switch (x) 
+            {
+                case 0:
+                    Console.WriteLine("End program");
+                    break;
+                case 1:
+                    if (a.IsFull())
+                    {
+                        Console.WriteLine("Array is Full You can use option 9");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Array not full");
+                    }
+                    break;
+                case 2:
+                    if (a.IsEmpty())
+                    {
+                        Console.WriteLine("Array is Empty");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Array has elements");
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Enter value : ");
+                    container = Convert.ToInt32(Console.ReadLine());
+                    a.Append(container);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter value : ");
+                    container = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter position");
+                    index = Convert.ToInt32(Console.ReadLine());
+                    index--;
+                    a.insert(container,index);
+                    break;
+                case 5:
+                    a.sortArray();
+                    Console.WriteLine("Array is sorted");
+                    break;
+                case 6:
+                    Console.WriteLine("Enter value to find it");
+                    container = Convert.ToInt32(Console.ReadLine());
+                    a.Search(container);
+                    break;
+                case 7:
+                    Console.WriteLine("you remove the last element");
+                    a.Remove();
+                    break;
+                case 8:
+                    Console.WriteLine("Enter value you want delete it");
+                    container = Convert.ToInt32(Console.ReadLine());
+                    a.Delete(container);
+                    break;
+                case 9:
+                    Console.WriteLine("Enter size : ");
+                    container = Convert.ToInt32(Console.ReadLine());
+                    a.IncreaseSize(container);
+                    break;
+                case 10:
+                    Console.WriteLine("__________________________");
+                    a.printArray();
+                    Console.WriteLine("__________________________");
+                    break ;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        } while (x!=0);
+    }
+    public static void showOptions() 
+    {
+        Console.WriteLine("_______________________________");
+        Console.WriteLine("1-IsFull");
+        Console.WriteLine("2-IsEmpty");
+        Console.WriteLine("3-Append");
+        Console.WriteLine("4-Insert");
+        Console.WriteLine("5-Sort");
+        Console.WriteLine("6-Search");
+        Console.WriteLine("7-Remove");
+        Console.WriteLine("8-Delete");
+        Console.WriteLine("9-Increase size");
+        Console.WriteLine("10-Print Array");
+        Console.WriteLine("0-Exit");
+        Console.WriteLine("_______________________________");
+    }
+}
